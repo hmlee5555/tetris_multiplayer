161688f4ae8d52b9848ea8966441634838d67c9d
hmlee5555 <hominlee@live.co.kr>
2021-02-13 23:36:39 +0900

    Create/Join Session

--- public/javascripts/index-page.js
+++ public/javascripts/index-page.js
@@ -1,6 +1,8 @@
 // const purple = "#7600FF";
 // const lightgrey = "#C1C1C1";
 // const white = "#fff";
+const loginModal = document.querySelector('#login-modal');
+const sessionModal = document.querySelector('#session-modal');
 
 document.querySelectorAll('.menu').forEach(item => {
     if (item.id === "quickmatch"){
@@ -9,31 +11,32 @@ document.querySelectorAll('.menu').forEach(item => {
             window.location.href = '/game';
         });
     }else if (item.id === "login") {
-        //login button
+        //login button : 로그인 모달 띄우기
         item.addEventListener('click', () => {
-            // 로그인 모달 띄우기??
-            toggleModal();
+            toggleModal(loginModal);
         });
     }else{
         //CREATE/JOIN SESSION button
-        item.addEventListener('click', event => {
-
+        item.addEventListener('click', () => {
+            toggleModal(sessionModal);
         });
     }
 });
 
+//모달 밖 영역 클릭하면 모달 닫기
 document.querySelectorAll('.overlay').forEach(item => {
     item.addEventListener('click', () => {
-        toggleModal();
+        // overlay에 클릭하므로 그의 parentNode가 modal-wrapper
+        toggleModal(item.parentNode);
     });
 });
 
-
-function toggleModal(){
-    let modal = document.querySelector(".modal-wrapper");
-    if (modal.style.display === "none"){
-        modal.style.display = "flex";
+// 모달 toggle: wrapper HTML elemnet를 받음
+function toggleModal(wrapper){
+    if (wrapper.style.display === "none"){
+        wrapper.style.display = "flex";
     }else{
-        modal.style.display = "none";
+        wrapper.style.display = "none";
+        wrapper.querySelector('.modal-error').innerHTML = "";
     }
 }
